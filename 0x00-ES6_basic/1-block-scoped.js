41ba622956383ce9f692ddef6cf43ee0b3f2dc10
Benson Kamau <benkabenson@gmail.com>
2023-04-17 20:11:05 +0000

    removed lint errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,13 @@
-export default function taskBlock (trueOrFalse) {
-  var task = false;
-  var task2 = true;
+#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
   if (trueOrFalse) {
-    var task = false;
-    var task2 = true;
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
+
   return [task, task2];
 }
