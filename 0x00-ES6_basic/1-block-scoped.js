a94b017579f3ab59ed61ffa4d0123bd1995483ab
Benson Kamau <benkabenson@gmail.com>
2023-04-17 18:23:08 +0000

    fixed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,7 +2,9 @@ export default function taskBlock (trueOrFalse) {
   var task = false;
   var task2 = true;
   if (trueOrFalse) {
+     //eslint-disable-next-line no-unused-vars
     var task = false;
+    //eslint-disable-next-line no-unused-vars
     var task2 = true;
   }
   return [task, task2];
