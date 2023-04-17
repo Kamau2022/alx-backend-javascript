036e3086453db7e1d2c97e381c8ba97ebb7c8b34
Benson Kamau <benkabenson@gmail.com>
2023-04-17 18:26:08 +0000

    used const in place of var

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock (trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
   if (trueOrFalse) {
-     //eslint-disable-next-line no-unused-vars
-    var task = false;
-    //eslint-disable-next-line no-unused-vars
-    var task2 = true;
+    const task = true;
+    const task2 = false;
   }
   return [task, task2];
 }
