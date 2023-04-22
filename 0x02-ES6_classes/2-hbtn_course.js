7803d1b8cd279a5677de01bd3f28694d95599def
Benson Kamau <benkabenson@gmail.com>
2023-04-22 22:02:20 +0000

    fixed

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
+    this.name = name;
     this.length = length;
     this.students = students;
   }
