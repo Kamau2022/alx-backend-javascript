33f0e9c63269499f7d5977b6fcd8e6e51a31dc65
Benson Kamau <benkabenson@gmail.com>
2023-04-25 16:14:09 +0000

    mapping done

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+export default function groceriesList() {
+  const myArray = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+
+  ];
+
+  const myMap = new Map(myArray);
+  return myMap;
+}
