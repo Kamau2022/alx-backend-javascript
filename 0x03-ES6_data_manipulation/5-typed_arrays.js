0410abec6963c57ccc6b39dd0d0422465800dc58
Benson Kamau <benkabenson@gmail.com>
2023-04-25 20:01:30 +0000

    an if statement added

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const dataview = new DataView(new ArrayBuffer(length));
-  dataview.setInt8(position, value);
-  return dataview;
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  } else {
+    const dataview = new DataView(new ArrayBuffer(length));
+    dataview.setInt8(position, value);
+    return dataview;
+  }
 }
