9a26e63b30ccc6f65992252fd7b3ab60fb35d8cb
Benson Kamau <benkabenson@gmail.com>
2023-06-13 08:45:18 +0000

    task done

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+
+  console.log(`Your name is ${name}!`);
+  process.stdin.pause();
+});
+
+process.stdin.on('pause', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
+
