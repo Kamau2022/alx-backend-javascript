bc44a80b9019074499f66c12c99dc61469361a3d
Benson Kamau <benkabenson@gmail.com>
2023-06-14 07:32:01 +0000

    Added an HTTP Header

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 }).listen(1245);
+module.exports = app;
