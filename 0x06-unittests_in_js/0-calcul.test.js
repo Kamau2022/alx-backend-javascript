f7b787634b4e14adbeae5deaa9cdbf59f305138e
Benson Kamau <benkabenson@gmail.com>
2023-06-15 18:40:25 +0000

    added module.exports = calculateNumber

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,11 +3,6 @@ const calculateNumber = require('./0-calcul');
 describe("calculate function", () => {
   it("sum of 1 and 3 should be 4", () => {
    assert.equal(calculateNumber(1, 3), 4);
-   calculateNumber(1, 3);
   });
  
-  it("sum of 1 and 3.7 should be 5", () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-    calculateNumber(1, 3.7);
-  });
 });
