5f0028b42d97a2325948f3346c11616e59d2e235
Benson Kamau <benkabenson@gmail.com>
2023-06-15 21:08:58 +0000

    to be fixed

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,18 @@
+const assert = require("assert");
+const calculateNumber = require('./0-calcul');
+describe("SUM", () => {
+  if (type == 'SUM'){
+  } it("sum of 1 and 3 should be 4", () => {
+   assert.equal(calculateNumber('SUM', 1, 3), 4);
+  });
+describe("SUBTRACT", () => {
+  it("difference of 5 and 3 should be 2", () => {
+   assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
+  }); 
+describe("DIVIDE", () => {
+  it("division of 25 and 5 should be 5", () => {
+   assert.equal(calculateNumber('DIVIDE', 25, 5), 5);
+});
+});
+});
+});
