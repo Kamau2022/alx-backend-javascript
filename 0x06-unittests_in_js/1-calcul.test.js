0c0978084b68deeb04a5605bac8e3dcc25c47ad3
Benson Kamau <benkabenson@gmail.com>
2023-06-16 06:33:30 +0000

    to be fixed

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,18 +1,23 @@
 const assert = require("assert");
 const calculateNumber = require('./0-calcul');
-describe("SUM", () => {
-  if (type == 'SUM'){
-  } it("sum of 1 and 3 should be 4", () => {
-   assert.equal(calculateNumber('SUM', 1, 3), 4);
+
+describe("calculateNumber", () => {
+  describe("SUM", () => {
+    it("should return the sum of two numbers", () => {
+      assert.equal(calculateNumber('SUM', 1, 3), 4);
+    });
   });
-describe("SUBTRACT", () => {
-  it("difference of 5 and 3 should be 2", () => {
-   assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
-  }); 
-describe("DIVIDE", () => {
-  it("division of 25 and 5 should be 5", () => {
-   assert.equal(calculateNumber('DIVIDE', 25, 5), 5);
+
+  describe("SUBTRACT", () => {
+    it("should return the difference of two numbers", () => {
+      assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
+    });
+  });
+
+  describe("DIVIDE", () => {
+    it("should return the division of two numbers", () => {
+      assert.equal(calculateNumber('DIVIDE', 25, 5), 5);
+    });
+  });
 });
-});
-});
-});
+
