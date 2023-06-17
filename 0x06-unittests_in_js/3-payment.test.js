b687b07104c19af106f25da0016ddc962a859187
Benson Kamau <benkabenson@gmail.com>
2023-06-17 19:39:18 +0000

    added sinon.restore method

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,10 +1,13 @@
 const sinon = require("sinon");
 const assert = require('assert');
+const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./3-payment');
 describe("SUM", () => {
   it("spy Utils.calculateNumber", () => {
-   const spy = sinon.spy();
+   const spy = sinon.spy(Utils);
    sendPaymentRequestToApi(100, 20);
+   expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+   expect(spy.calculateNumber.callCount).to.be.equal(1);
    sinon.restore();
   });
 });
