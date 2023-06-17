167c02558f8998dd5d5362b5bbee45110469e600
Benson Kamau <benkabenson@gmail.com>
2023-06-17 20:01:22 +0000

    const assert = require('assert');

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,11 @@
 const sinon = require("sinon");
-const assert = require('assert');
 const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./3-payment');
 describe("SUM", () => {
   it("spy Utils.calculateNumber", () => {
    const spy = sinon.spy(Utils);
    sendPaymentRequestToApi(100, 20);
-   expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+   expect(spy.calculateNumber.calledWith('SUM', 100, 20));
    sinon.restore();
   });
 });
