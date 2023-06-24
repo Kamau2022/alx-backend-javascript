f304f31054ce15dc75da0c820cf19cd7e9556071
Benson Kamau <benkabenson@gmail.com>
2023-06-24 21:59:56 +0000

    made to pass lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,64 +1,55 @@
-const fs = require("fs");
+const fs = require('fs');
 
-function countStudents(){
+function countStudents() {
+// Importing fs module to read the file
+  const data = fs.readFileSync('database.csv', 'utf8'); // using 'utf8' encoding
 
-// Importing fs module to read the file 
-const fs = require("fs")
-const data = fs.readFileSync("database.csv", "utf8") // using 'utf8' encoding
+  // splitting the CSV by row as "\n" represents new row
+  const rows = data.split('\n');
 
+  // Getting the attribute from the index 0 of the rows
+  const getAttribute = () => rows[0].split(',');
 
-// splitting the CSV by row as "\n" represents new row
-let rows = data.split("\n") 
+  // Further splitting the Rows
+  const getRowData = () => {
+    const rowData = [];
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i].split(',');
+      rowData.push(row);
+    }
+    return rowData;
+  };
 
-// Getting the attribute from the index 0 of the rows
-const getAttribute = () => {
-  return rows[0].split(',')
-}
+  //  The CSV parser
+  const CSVObject = () => {
+    const attributes = getAttribute(data);
+    const rowData = getRowData();
 
-// Further splitting the Rows 
-const getRowData = () => {
-  let rowData = []
-  for (let i = 1; i < rows.length; i++) {
-    const row = rows[i].split(',')
-    rowData.push(row)
-  }
-  return rowData;
-}
+    const rowsObj = [];
+    for (let i = 0; i < rowData.length; i += 1) {
+      const rowObj = {};
 
+      for (let j = 0; j < rowData[i].length; j += 1) {
+        rowObj[attributes[j]] = rowData[i][j]; // converting to object as attribute as key.
+      }
 
-//  The CSV parser 
-const CSVToObject = () => {
-
-  const attributes = getAttribute(data)
-  const rowData = getRowData()
-
-  let rowsObj = []
-  for (let i = 0; i < rowData.length; i++) {
-
-    let rowObj = {}
-
-    for (let j = 0; j < rowData[i].length; j++) {
-      rowObj[attributes[j]] = rowData[i][j]; // converting to object as attribute as key.
+      rowsObj.push(rowObj);
     }
 
-    rowsObj.push(rowObj)
-  }
+    return rowsObj;
+  };
 
-  return rowsObj
-}
+  const newData = CSVObject();
+  const FirstNamesCs = newData
+    .filter((person) => person.field === 'CS')
+    .map((person) => person.firstname);
 
-let newData = CSVToObject()
-const FirstNames_cs = newData
-  .filter(person => person.field === 'CS')
-  .map(person => person.firstname)
-  
-const FirstNames_swe = newData
-  .filter(person => person.field === 'SWE')
-  .map(person => person.firstname)
+  const FirstNamesSwe = newData
+    .filter((person) => person.field === 'SWE')
+    .map((person) => person.firstname);
 
-console.log(`Number of students: ${FirstNames_swe.length + FirstNames_cs.length}`);
-console.log(`Number of students in CS: ${FirstNames_cs.length}. List: ${FirstNames_cs.join(', ')}`)
-console.log(`Number of students in SWE: ${FirstNames_swe.length}. List: ${FirstNames_swe.join(', ')}`)
-
+  console.log(`Number of students: ${FirstNamesSwe.length + FirstNamesCs.length}`);
+  console.log(`Number of students in CS: ${FirstNamesCs.length}. List: ${FirstNamesCs.join(', ')}`);
+  console.log(`Number of students in SWE: ${FirstNamesSwe.length}. List: ${FirstNamesSwe.join(', ')}`);
 }
 module.exports = countStudents;
